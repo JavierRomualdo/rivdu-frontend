1dd37ea56a446658bb7f1f500d6f7fdd2bd5fbd4
unknown <yimmyga>
2018-03-05 16:15:51 -0500

    buscar gerente por dni

--- src/app/empresa/modal-empresa/modal-empresa.component.ts
+++ src/app/empresa/modal-empresa/modal-empresa.component.ts
@@ -4,6 +4,12 @@ import { ApiRequestService } from '../../servicios/api-request.service';
 import { ToastrService } from 'ngx-toastr';
 import { LS } from '../../app-constants';
 import { Empresa } from '../../entidades/entidad.empresa';
+import {Persona} from '../../entidades/entidad.persona';
+import {EmpresaComponent} from '../../empresa/empresa.component';
+import {ModalGerenteComponent} from '../../empresa/modal-gerente/modal-gerente.component';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { ModalUbigeoComponent } from '../../mantenimiento-captacion/modal-ubigeo/modal-ubigeo.component';
+
 
 @Component({
   selector: 'app-modal-empresa',
@@ -14,13 +20,18 @@ export class ModalEmpresaComponent implements OnInit {
 
   public ruc:string;
   public empresa:Empresa;
+  public persona:Persona;
+
+
 
   constructor(
     public activeModal: NgbActiveModal,
     public api: ApiRequestService,
-    public toastr: ToastrService
+    public toastr: ToastrService,
+    private modalService: NgbModal
   ) {
     this.empresa = new Empresa();
+    this.persona= new Persona();
   }
 
   ngOnInit() {
@@ -38,6 +49,20 @@ export class ModalEmpresaComponent implements OnInit {
         }
       })
       .catch(err => this.handleError(err));
+  }
+  abrirpersona():void{
+    const modalRef = this.modalService.open(ModalGerenteComponent, {size: 'lg', keyboard: false});
+    modalRef.result.then((result) => {
+    }, (reason) => {
+    });
+  }
+
+
+  ubigeo():void{
+    const modalRef = this.modalService.open(ModalUbigeoComponent, {size: 'lg', keyboard: false});
+    modalRef.result.then((result) => {
+    }, (reason) => {
+    });
   }
 
   actualizarEmpresa(){
