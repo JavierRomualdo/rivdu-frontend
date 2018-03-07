e25ea9eec1f4acc1eecc730c37045701188deda8
Christhian <christhian2524@gmail.com>
2018-03-07 13:05:57 -0500

    AgregarProgramaModal

--- src/app/empresa/modal-programas/modal-programas.component.ts
+++ src/app/empresa/modal-programas/modal-programas.component.ts
@@ -12,16 +12,34 @@ import { Programas } from '../../entidades/entidad.programas';
 })
 export class ModalProgramasComponent implements OnInit {
 
-  public programa:Programas;
+    public programa:Programas;
+    public cargando:boolean=false;
 
-  constructor(
-    public activeModal: NgbActiveModal,
-    public api: ApiRequestService,
-    public toastr: ToastrService
-  ) { }
+    constructor(public activeModal: NgbActiveModal,
+                public api: ApiRequestService,
+                public toastr: ToastrService) {
+        this.programa = new Programas();
+    }
 
-  ngOnInit() {
-    this.programa = new Programas();
-  }
+    guardarProgramas(){
+        this.cargando=true;
+        this.api.post("programas",this.programa)
+            .then(respuesta => {
+                if(respuesta && respuesta.extraInfo){
+                    this.programa = respuesta.extraInfo;
+                    this.toastr.success("Registro guardado exitosamente", 'Exito');
+                    this.activeModal.close(this.programa);
+                    this.cargando=false;
+                } else {
+                    this.cargando=false;
+                    this.toastr.error(respuesta.operacionMensaje, 'Error');
+                }
+            })
+            .catch(err => this.handleError(err));
+    }
+
+    private handleError(error: any): void {
+        this.toastr.error("Error Interno", 'Error');
+    }
 
 }
