04451f160f5bccdcf98a6d3022e939432575792d
Christhian <christhian2524@gmail.com>
2018-03-13 17:25:52 -0500

    DatosDeConstrucion

--- src/app/mantenimiento-construccion/modal-labores/modal-labores.component.ts
+++ src/app/mantenimiento-construccion/modal-labores/modal-labores.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbActiveModal, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {ApiRequestService} from '../../servicios/api-request.service';
+import {ToastrService} from 'ngx-toastr';
+import {Labores} from '../../entidades/entidad.labores';
 
 @Component({
   selector: 'app-modal-labores',
@@ -7,9 +11,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalLaboresComponent implements OnInit {
 
-  constructor() { }
+    public cambiartitulo:boolean=false;
+    public clicknuevo:boolean=false;
+    public cargando:boolean=false;
+    public listado:boolean=false;
+    public  lista:any=[];
+
+    public labores:Labores ;
+
+  constructor(public activeModal: NgbActiveModal, public api: ApiRequestService,
+              public toastr: ToastrService, public modal: NgbModal) {
+
+      this.labores = new Labores();
+  }
 
   ngOnInit() {
+    this.listarModalLabores();
   }
+
+
+    listarModalLabores(){
+        this.cargando=true;
+        this.api.get("labores/listar")
+            .then(respuesta => {
+                if(respuesta && respuesta.extraInfo){
+                    this.lista = respuesta.extraInfo;
+                    this.cargando=false;
+                } else {
+                    this.toastr.error(respuesta.operacionMensaje, 'Error');
+                    this.cargando = false;
+                }
+            })
+            .catch(err => this.handleError(err));
+
+    };
+
+    abriNuevoModalNuevo(){
+        this.clicknuevo=true;
+        this.cambiartitulo=true;
+
+    };
+
+    abrirModalListado(){
+        this.clicknuevo=false;
+        this.cambiartitulo=false;
+
+    };
+
+    guardarDatosNuevoModal(){
+        this.cargando=true;
+        this.api.post("labores",this.labores)
+            .then(respuesta => {
+                if(respuesta && respuesta.extraInfo){
+                    this.labores = respuesta.extraInfo;
+                    this.toastr.success("Registro guardado exitosamente", 'Exito');
+                    this.cargando = false;
+                    this.listado = true;
+                    this.abrirModalListado();
+                    this.listarModalLabores();
+
+                } else {
+                    this.cargando=false;
+                    this.toastr.error(respuesta.operacionMensaje, 'Error');
+
+                }
+            })
+            .catch(err => this.handleError(err));
+    };
+
+    private handleError(error: any): void {
+        this.toastr.error("Error Interno", 'Error');
+        this.cargando = false;
+    }
 
 }
--- src/app/mantenimiento-construccion/modal-materiales/modal-materiales.component.ts
+++ src/app/mantenimiento-construccion/modal-materiales/modal-materiales.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbActiveModal, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {ApiRequestService} from '../../servicios/api-request.service';
+import {ToastrService} from 'ngx-toastr';
+import {Materiales} from '../../entidades/entidad.materiales';
+import {ConfirmacionComponent} from '../../util/confirmacion/confirmacion.component';
 
 @Component({
   selector: 'app-modal-materiales',
@@ -7,9 +12,98 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalMaterialesComponent implements OnInit {
 
-  constructor() { }
+    public cambiartitulo:boolean=false;
+    public clicknuevo:boolean=false;
+    public cargando:boolean=false;
+    public listado:boolean=false;
+    public  lista:any=[];
+
+    public materiales:Materiales ;
+
+
+  constructor(public activeModal: NgbActiveModal, public api: ApiRequestService,
+              public toastr: ToastrService, public modal: NgbModal) {
+
+      this.materiales = new Materiales();
+}
 
   ngOnInit() {
+    this.listarModalMateriales();
   }
+
+    listarModalMateriales(){
+        this.cargando=true;
+        this.api.get("materiales/listar")
+            .then(respuesta => {
+                if(respuesta && respuesta.extraInfo){
+                    this.lista = respuesta.extraInfo;
+                    this.cargando=false;
+                } else {
+                    this.toastr.error(respuesta.operacionMensaje, 'Error');
+                    this.cargando = false;
+                }
+            })
+            .catch(err => this.handleError(err));
+
+    };
+
+    abriNuevoModalNuevo(){
+        this.clicknuevo=true;
+        this.cambiartitulo=true;
+
+    };
+
+    abrirModalListado(){
+        this.clicknuevo=false;
+        this.cambiartitulo=false;
+
+    };
+
+    guardarDatosNuevoModal(){
+        this.cargando=true;
+        this.api.post("materiales",this.materiales)
+            .then(respuesta => {
+                if(respuesta && respuesta.extraInfo){
+                    this.materiales = respuesta.extraInfo;
+                    this.toastr.success("Registro guardado exitosamente", 'Exito');
+                    this.cargando = false;
+                    this.listado = true;
+                    this.abrirModalListado();
+                    this.listarModalMateriales();
+
+                } else {
+                    this.cargando=false;
+                    this.toastr.error(respuesta.operacionMensaje, 'Error');
+
+                }
+            })
+            .catch(err => this.handleError(err));
+    };
+
+    eliminarEstado(li){
+        this.api.delete("materiales/eliminarEstadoMaterial/"+li.id)
+            .then(respuesta => {
+                if(respuesta && respuesta.extraInfo){
+                    this.lista.splice(this.lista.lastIndexOf(li),1);
+                } else {
+                    this.toastr.error(respuesta.operacionMensaje, 'Error');
+                }
+            })
+            .catch(err => this.handleError(err));
+    };
+
+    confirmarEliminado(li): void {
+        const modalRef = this.modal.open(ConfirmacionComponent, {size: 'sm', keyboard: false});
+        modalRef.result.then((result) => {
+            this.eliminarEstado(li);
+            this.toastr.success("Registro eliminado exitosamente", 'Exito');
+        }, (reason) => {
+        });
+    };
+
+    private handleError(error: any): void {
+        this.toastr.error("Error Interno", 'Error');
+        this.cargando = false;
+    }
 
 }
