00eca53bdd678813708d0ed43bc86580a1a3e91c
Javier Romualdo <javierromualdo@hotmail.com>
2018-04-03 16:41:54 -0500

    final mantenimiento proyectos

--- src/app/captaciones-proyectos/captaciones-proyectos.component.ts
+++ src/app/captaciones-proyectos/captaciones-proyectos.component.ts
@@ -4,6 +4,9 @@ import { ModalProyectosformularioComponent } from './modal-proyectosformulario/m
 import { ApiRequestService } from '../servicios/api-request.service';
 import { ToastrService } from 'ngx-toastr';
 import { Paginacion } from '../entidades/entidad.paginacion';
+import { Proyecto } from '../entidades/entidad.proyecto';
+import { ConfirmacionComponent } from '../util/confirmacion/confirmacion.component';
+import { AuthService } from '../servicios/auth.service';
 
 @Component({
   selector: 'app-captaciones-proyectos',
@@ -11,36 +14,68 @@ import { Paginacion } from '../entidades/entidad.paginacion';
   styleUrls: ['./captaciones-proyectos.component.css']
 })
 export class CaptacionesProyectosComponent implements OnInit {
-
+  public confirmarcambioestado: boolean = false;
   public cargando: boolean = false;
   public page: number = 1;
   public paginacion: Paginacion;
-  public  lista:any=[];
+  public  lista: any = []; // lista proyecto
 
   constructor(
     private modalService: NgbModal,
     private api: ApiRequestService,
     private modal: NgbModal,
+    public auth: AuthService,
     private toastr: ToastrService
   ) {
     this.paginacion = new Paginacion();
   }
 
   ngOnInit() {
+    this.listarProyectos();
   }
 
   abrirProyecto(): void {
     const modalRef = this.modalService.open(ModalProyectosformularioComponent, {size: 'lg', keyboard: false});
     modalRef.result.then((result) => {
+      this.listarProyectos();
     }, (reason) => {
     });
   }
 
+  confirmarcambiodeestado(proyecto): void {
+        const modalRef = this.modal.open(ConfirmacionComponent, {windowClass: 'nuevo-modal', size: 'sm', keyboard: false});
+        modalRef.result.then((result) => {
+            this.confirmarcambioestado = true;
+            this.cambiarestadoproyecto(proyecto);
+            // this.auth.agregarmodalopenclass();
+        }, (reason) => {
+            proyecto.estado = !proyecto.estado;
+            // this.auth.agregarmodalopenclass();
+        });
+    }
+
+  cambiarestadoproyecto(proyecto: Proyecto) {
+    this.cargando = true;
+    return this.api.post('proyecto/eliminarconestado', {id: proyecto.id})
+        .then(
+            data => {
+                if(data && data.extraInfo) {
+                    this.toastr.success(data.operacionMensaje, 'Exito');
+                    this.listarProyectos();
+                } else {
+                    this.toastr.info(data.operacionMensaje, 'Informacion');
+                }
+                this.cargando = false;
+            }
+        )
+        .catch(err => this.handleError(err));
+  }
+
   listarProyectos() {
     this.cargando = true;
-        this.api.get("proyectos/listar")
+        this.api.get('proyecto/listar')
             .then(respuesta => {
-                if(respuesta && respuesta.extraInfo) {
+                if (respuesta && respuesta.extraInfo) {
                     this.lista = respuesta.extraInfo;
                     this.cargando = false;
                 } else {
@@ -49,5 +84,19 @@ export class CaptacionesProyectosComponent implements OnInit {
                 }
             })
             .catch(err => this.handleError(err));
+  }
+
+  traerParaEdicion(id) {
+        const modalRef = this.modal.open(ModalProyectosformularioComponent, {size: 'lg', keyboard: false});
+        modalRef.componentInstance.edit = id;
+        modalRef.result.then((result) => {
+            this.listarProyectos();
+        }, (reason) => {
+        });
+  }
+
+  private handleError(error: any): void {
+    this.cargando = false;
+      this.toastr.error('Error Interno', 'Error');
   }
 }
--- src/app/captaciones-proyectos/modal-proyectosformulario/modal-proyectosformulario.component.ts
+++ src/app/captaciones-proyectos/modal-proyectosformulario/modal-proyectosformulario.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from '../../servicios/auth.service';
 import {ApiRequestService} from '../../servicios/api-request.service';
 import {ToastrService} from 'ngx-toastr';
+import { Proyecto } from '../../entidades/entidad.proyecto';
 
 @Component({
   selector: 'app-modal-proyectosformulario',
@@ -10,20 +11,105 @@ import {ToastrService} from 'ngx-toastr';
   styleUrls: ['./modal-proyectosformulario.component.css']
 })
 export class ModalProyectosformularioComponent implements OnInit {
-
+  @Input() edit;
+  public proyecto: Proyecto;
+  public  lista: any = [];
+  public confirmarcambioproyecto = false;
+  public cargando: boolean = false;
+  public listado: boolean = false;
+  public vistaFormulario = false;
+  public verNuevo = false;
   constructor(
     public activeModal: NgbActiveModal,
     public authService: AuthService,
+    private apiRequest: ApiRequestService,
     public api: ApiRequestService,
     public auth: AuthService,
     private modalService: NgbModal,
     private modal: NgbModal,
     public toastr: ToastrService
-  ) { }
+  ) {
+    this.proyecto = new Proyecto();
+   }
 
   ngOnInit() {
+    if (this.edit) {
+      this.traerParaEdicion(this.edit);
+    }
   }
   guardarproyecto() {
+    this.cargando = true;
+    if (!this.proyecto.id) { // nuevo
+        this.api.post('proyecto', this.proyecto)
+          .then(respuesta => {
+            if (respuesta && respuesta.extraInfo) {
+              this.proyecto = respuesta.extraInfo;
+              this.toastr.success(respuesta.operacionMensaje, 'Exito');
+              this.activeModal.close(this.proyecto);
+              this.cargando = false;
+            } else {
+              this.cargando = false;
+              this.toastr.error(respuesta.operacionMensaje, 'Error');
+            }
+          })
+          .catch(err => this.handleError(err));
+    } else { // editar
+      return this.api.post('proyecto', this.proyecto)
+        .then(
+          data => {
+            if (data && data.extraInfo) {
+                this.cargando = false;
+                this.proyecto = data.extraInfo;
+                this.proyecto = new Proyecto();
+                this.toastr.success(data.operacionMensaje, 'Exito');
+                this.activeModal.close(this.proyecto);
+                this.cargando = false;
+              } else {
+                this.toastr.info(data.operacionMensaje, 'Informacion');
+                this.cargando = false;
+              }
+          }
+        )
+        .catch(err => this.handleError(err));
+    }
+  }
 
+  eliminarproyecto(li) {
+    this.cargando = true;
+    this.api.delete('proyecto/eliminarproyecto/' + li.id)
+        .then(respuesta => {
+            if (respuesta && respuesta.extraInfo) {
+                this.lista.splice(this.lista.lastIndexOf(li), 1);
+            } else {
+                this.toastr.error(respuesta.operacionMensaje, 'Error');
+            }
+          this.cargando = false;
+        })
+        .catch(err => this.handleError(err));
+  }
+
+  traerParaEdicion(id) {
+    this.cargando = true;
+    this.vistaFormulario = true;
+    this.verNuevo = true;
+    return this.apiRequest.post('proyecto/obtener', {id: id})
+      .then(
+        data => {
+          if (data && data.extraInfo){
+            this.cargando = false;
+            this.proyecto = data.extraInfo;
+          } else {
+            this.toastr.info(data.operacionMensaje, 'Informacion');
+            this.vistaFormulario = false;
+            this.cargando = false;
+          }
+        }
+      )
+      .catch(err => this.handleError(err));
+  }
+
+  private handleError(error: any): void {
+    this.cargando = false;
+      this.toastr.error('Error Interno', 'Error');
   }
 }
