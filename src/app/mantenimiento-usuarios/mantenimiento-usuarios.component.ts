a6975ee1b334b767af66d7a6e5659cfad68b81bb
kevinQuispe <kquispe07@hotmail.com>
2018-04-25 18:10:01 -0500

    roles usuarios

--- src/app/mantenimiento-usuarios/mantenimiento-usuarios.component.ts
+++ src/app/mantenimiento-usuarios/mantenimiento-usuarios.component.ts
@@ -3,42 +3,39 @@ import { Router } from '@angular/router';
 import { AuthService } from '../servicios/auth.service';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
-import {ModalRolesAccesoComponent} from "./modal-roles-acceso/modal-roles-acceso.component";
-import {ModalRolesComponent} from "./modal-roles/modal-roles.component";
+import { ModalRolesAccesoComponent } from "./modal-roles-acceso/modal-roles-acceso.component";
+import { ModalRolesComponent } from "./modal-roles/modal-roles.component";
 
 
 @Component({
-  selector: 'app-mantenimiento-usuarios',
-  templateUrl: './mantenimiento-usuarios.component.html',
-  styleUrls: ['./mantenimiento-usuarios.component.css']
+    selector: 'app-mantenimiento-usuarios',
+    templateUrl: './mantenimiento-usuarios.component.html',
+    styleUrls: ['./mantenimiento-usuarios.component.css']
 })
 export class MantenimientoUsuariosComponent implements OnInit {
 
-  constructor(
-      public authService: AuthService,
-      public router: Router,
-      public modalService: NgbModal,
-      private modal: NgbModal,
-      public auth: AuthService
-  ) { }
+    constructor(
+        public authService: AuthService,
+        public router: Router,
+        public modalService: NgbModal,
+        private modal: NgbModal,
+        public auth: AuthService
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-    abrirRoles():void{
-        const modalRef = this.modalService.open(ModalRolesComponent, {size: 'lg', keyboard: false});
+    abrirRoles(): void {
+        const modalRef = this.modalService.open(ModalRolesComponent, { size: 'lg', keyboard: false });
         modalRef.result.then((result) => {
         }, (reason) => {
         });
     }
-
-    abrirRolesPermiso():void{
-        const modalRef = this.modalService.open(ModalRolesAccesoComponent, {size: 'lg', keyboard: false});
+    //abrir modal roles permiso principal
+    abrirRolesPermiso(): void {
+        const modalRef = this.modalService.open(ModalRolesAccesoComponent, { size: 'sm', keyboard: false });
         modalRef.result.then((result) => {
         }, (reason) => {
         });
     }
-
-
-
 }
