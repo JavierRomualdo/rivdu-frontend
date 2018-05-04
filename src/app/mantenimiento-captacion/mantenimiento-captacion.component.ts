bf383bd04c2bb79d2121190b93242be25b988f4a
MarioMario <marioramos083@gmail.com>
2018-05-04 12:26:59 -0500

    delete captador

--- src/app/mantenimiento-captacion/mantenimiento-captacion.component.ts
+++ src/app/mantenimiento-captacion/mantenimiento-captacion.component.ts
@@ -5,105 +5,56 @@ import { ModalEstadocivilComponent } from "./modal-estadocivil/modal-estadocivil
 import { ModalRelacionpersonalComponent } from "./modal-relacionpersonal/modal-relacionpersonal.component";
 import { ModalUbigeoComponent } from "./modal-ubigeo/modal-ubigeo.component";
 import { ModalIngenierosComponent } from '../empresa/modal-ingenieros/modal-ingenieros.component';
-import {ApiRequestService} from '../servicios/api-request.service';
-import {Persona} from '../entidades/entidad.persona';
-import {ToastrService} from 'ngx-toastr';
-import {Paginacion} from '../entidades/entidad.paginacion';
-import {Ubigeo} from '../entidades/entidad.ubigeo';
+import { ApiRequestService } from '../servicios/api-request.service';
+import { Persona } from '../entidades/entidad.persona';
+import { ToastrService } from 'ngx-toastr';
+import { Paginacion } from '../entidades/entidad.paginacion';
+import { Ubigeo } from '../entidades/entidad.ubigeo';
 
 @Component({
-  selector: 'app-mantenimiento-captacion',
-  templateUrl: './mantenimiento-captacion.component.html',
-  styleUrls: ['./mantenimiento-captacion.component.css']
+    selector: 'app-mantenimiento-captacion',
+    templateUrl: './mantenimiento-captacion.component.html',
+    styleUrls: ['./mantenimiento-captacion.component.css']
 })
 export class MantenimientoCaptacionComponent implements OnInit {
 
-    public page: number = 1;
-    public paginacion: Paginacion;
-    public cargando:boolean= false;
-    public captador : Persona;
-    public  listacaptadores:any=[];
-    public parametros:any={};
-    public captadores:Persona[];
-    public dni:string="";
-    public nombre:string="";
-  constructor(
-    private modalService: NgbModal, public api: ApiRequestService, public toastr: ToastrService
-  ) {
-      this.captadores= [];
-      this.paginacion = new Paginacion();
-      this.captador= new Persona();
-      this.captador.idubigeo = new Ubigeo();
-  }
+    public cargando: boolean = false;
 
-  ngOnInit() {
-    this.listarCaptadores();
-  }
+    constructor(
+        private modalService: NgbModal, public api: ApiRequestService, public toastr: ToastrService
+    ) {
+    }
 
-  abrirUbigeo():void{
-    const modalRef = this.modalService.open(ModalUbigeoComponent, {size: 'lg', keyboard: false});
-    modalRef.result.then((result) => {
-    }, (reason) => {
-    });
-  }
+    ngOnInit() {
+    }
 
-  abrirDescuentos():void{
-    const modalRef = this.modalService.open(ModalEspecificacionesComponent, {size: 'lg', keyboard: false});
-    modalRef.result.then((result) => {
-    }, (reason) => {
-    });
-  }
+    abrirUbigeo(): void {
+        const modalRef = this.modalService.open(ModalUbigeoComponent, { size: 'lg', keyboard: false });
+        modalRef.result.then((result) => {
+        }, (reason) => {
+        });
+    }
 
-  abrirEstado():void{
-    const modalRef = this.modalService.open(ModalEstadocivilComponent, {size: 'sm', keyboard: false});
-    modalRef.result.then((result) => {
-    }, (reason) => {
-    });
-  }
+    abrirDescuentos(): void {
+        const modalRef = this.modalService.open(ModalEspecificacionesComponent, { size: 'lg', keyboard: false });
+        modalRef.result.then((result) => {
+        }, (reason) => {
+        });
+    }
 
-  abrirRelacion():void{
-    const modalRef = this.modalService.open(ModalRelacionpersonalComponent, {size: 'sm', keyboard: false});
-    modalRef.result.then((result) => {
-    }, (reason) => {
-    });
-  }
+    abrirEstado(): void {
+        const modalRef = this.modalService.open(ModalEstadocivilComponent, { size: 'sm', keyboard: false });
+        modalRef.result.then((result) => {
+        }, (reason) => {
+        });
+    }
 
-    listarcaptadores(){
-        this.api.get("captadores/listar")
-            .then(respuesta => {
-                if(respuesta && respuesta.extraInfo){
-                    this.listacaptadores = respuesta.extraInfo;
-                } else {
-                    this.toastr.error(respuesta.operacionMensaje, 'Error');
-                }
-            })
-            .catch(err => this.handleError(err));
-
-    };
-
-    listarCaptadores(){
-        this.cargando= true;
-        this.api.post('captador/pagina/'+this.page+'/cantidadPorPagina/'+this.paginacion.cantidadPorPagina, this.parametros)
-            .then(data => {
-                if(data){
-                    this.cargando = false;
-                    this.paginacion.totalRegistros = data.totalRegistros;
-                    this.paginacion.paginaActual = data.paginaActual;
-                    this.paginacion.totalPaginas = data.totalPaginas;
-                    this.captadores = data.registros;
-                }
-            })
-            .catch(err => this.handleError(err));
-    };
-    busqueda(): void {
-        this.page = 1;
-        this.parametros = {
-            "dni":this.dni,
-            "nombre":this.nombre
-        };
-        this.listarCaptadores();
-
-    };
+    abrirRelacion(): void {
+        const modalRef = this.modalService.open(ModalRelacionpersonalComponent, { size: 'sm', keyboard: false });
+        modalRef.result.then((result) => {
+        }, (reason) => {
+        });
+    }
 
     private handleError(error: any): void {
         this.toastr.error("Error Interno", 'Error');
